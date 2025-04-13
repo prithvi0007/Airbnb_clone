cecc028d15eeede1c1a3723c84428e781545b69b
prithvi raj <136835051+prithvi0007@users.noreply.github.com>
2025-04-13 17:10:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -116,6 +116,11 @@ async function getCoordinates(location) {
   }
 }
 
-app.listen(8080, () => {
-  console.log("server is listening to port 8080");
+//app.listen(8080, () => {
+ // console.log("server is listening to port 8080");
+//});
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
 });
+
